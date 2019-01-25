f2c35fe420ec7d66ff5821d16d61cb56d93580db
Ian King <ian.antking@gmail.com>
2019-01-25 20:50:42 +0000

    converted AddScreen to stateful component

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import {
+  StyleSheet,
   View,
   Text,
 } from 'react-native';
 
-export default () => (
-  <View>
-    <Text>
-      This is the Add screen!
-    </Text>
-  </View>
-);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+class AddScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          This is the Add screen!
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default AddScreen;
