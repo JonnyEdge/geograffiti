d8f9adb30d735c209e8680c8b89ef8e6877c012c
Ian King <ian.antking@gmail.com>
2019-01-25 20:01:09 +0000

    MapScreen now tracks user location and displays on map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import {
   MapView,
+  Location,
 } from 'expo';
 
-export default (screenProps) => {
-  return (
-    <MapView
-      style={{ flex: 1 }}
-      initialRegion={{
-        latitude: parseFloat(screenProps.location.latitude),
-        longitude: parseFloat(screenProps.location.longitude),
-        latitudeDelta: 0.0043,
-        longitudeDelta: 0.0034,
-      }}
-    />
-  );
-};
+class MapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: parseFloat(this.props.location.latitude),
+        longitude: parseFloat(this.props.location.longitude),
+      },
+    };
+  }
+
+  componentDidMount() {
+    Location.watchPositionAsync({
+      distanceInterval: 1,
+      accuracy: 6,
+    }, (location) => {
+      console.log(location);
+      this.setState({
+        region: {
+          latitude: parseFloat(location.coords.latitude),
+          longitude: parseFloat(location.coords.longitude),
+        },
+      });
+    });
+  }
+
+  render() {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        region={{
+          latitude: this.state.region.latitude,
+          longitude: this.state.region.longitude,
+          latitudeDelta: 0.0043,
+          longitudeDelta: 0.0034,
+        }}
+        followsUserLocation
+        showsUserLocation
+      />
+    );
+  }
+}
+
+export default MapScreen;
