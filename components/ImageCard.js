a9bba98fcf076ba5ae8d92ff3ceccc8ec2bb8ec7
Jonny Edge <jonnyedge@outlook.com>
2019-02-08 21:20:29 +0000

    Persistent header through React Navigation instead of React Elements.

--- components/ImageCard.js
+++ components/ImageCard.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Image, Text, Dimensions } from 'react-native';
+import { Image, View, Text, Dimensions } from 'react-native';
 
 const dimensions = Dimensions.get('window');
 const imageHeight = dimensions.height;
 const imageWidth = dimensions.width;
 
 export default (props) => (
-  <Image
-    style={{ width: imageWidth, height: imageHeight }}
-    source={{ uri: props.url }}
-  />
+  <React.Fragment>
+    <View>
+      <Image
+        style={{ width: imageWidth, height: imageHeight }}
+        source={{ uri: props.url }}
+      />
+    </View>
+  </React.Fragment>
 );
 
 // import {
